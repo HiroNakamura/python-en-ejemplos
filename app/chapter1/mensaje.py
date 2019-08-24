a3c27e58fe1c2d3d35ac097e4d00b00122f152b7
ARIEL <carraro.ariel@gmail.com>
2019-08-24 17:58:26 -0500

    Se edito mensaje

--- app/chapter1/mensaje.py
+++ app/chapter1/mensaje.py
@@ -1,14 +1,6 @@
 #!/bin/python
 # coding=utf-8
 
-import PyZenity
-#from PyZenity import InfoMessage
-import os
-
-def get_mensaje():
-	print "\n*** PyZenity ***"
-	print os.environ['PATH']
-	#InfoMessage('Hola')
-	PyZenity.InfoMessage('Usando una ventana de informacion')
-	
-	
+'''
+PARA MONGODB
+'''
