a5511398b725f939983bbc4815f9695f25cdf8e9
Fer_Carraro <carraro.fernando@gmail.com>
2019-08-24 16:46:32 -0500

    Se edito mensaje.py

--- app/chapter1/mensaje.py
+++ app/chapter1/mensaje.py
@@ -2,10 +2,13 @@
 # coding=utf-8
 
 import PyZenity
-
+from PyZenity import InfoMessage
+import os
 
 def get_mensaje():
 	print "\n*** PyZenity ***"
-	#PyZenity.InfoMessage('Usando una ventana de informacion')
+	print os.environ['PATH']
+	InfoMessage('Hola')
+	PyZenity.InfoMessage('Usando una ventana de informacion')
 	
 	
