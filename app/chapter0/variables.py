1cd979541dcf7e237ad8511d353112222830286b
Fer_Carraro <carraro.fernando@gmail.com>
2019-08-24 14:44:33 -0500

    Se añadieron cambios

---
+++ app/chapter0/variables.py
@@ -0,0 +1,14 @@
+#!/bin/python
+# coding=utf-8
+
+
+def funcion_variables():
+	entero = 33
+	character = 'A'
+	cadena = str("Python for Beginners")
+	booleana = 34 > 3 #True
+	booleana = not booleana #False
+	print "Entero = ",entero
+	print "Caracter = ",character
+	print "Cadena = ",cadena
+	print "Booleana = ",booleana #False
--- app/main.py
+++ app/main.py
@@ -2,8 +2,12 @@
 # coding=utf-8
 
 
+import chapter0.variables
+
+
 def main():
 	print "Hola, mundo"
+	variables()
 
 
 if __name__ == '__main__':
