969128c001a8533ef76275bd9e597b230f5454d3
ARIEL <carraro.ariel@gmail.com>
2019-08-25 13:15:18 -0500

    Se editó cadenas.py

--- app/chapter0/cadenas.py
+++ app/chapter0/cadenas.py
@@ -1,3 +1,10 @@
-#
+#!/bin/python
+# coding=utf-8
 
 CADENA_GLOBAL = "Python for Beginners"
+
+def cadenas():
+  global CADENA_GLOBAL
+  print CADENA_GLOBAL
+  print "Tamaño: ",len(CADENA_GLOBAL)
+  print "Minusculas: ",CADENA_GLOBAL.lower()
