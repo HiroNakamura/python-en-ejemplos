b583c6d9decb96368c7132006bacbc37a90b4432
ARIEL <carraro.ariel@gmail.com>
2019-08-25 10:51:05 -0500

    Se creó archivo funciones

---
+++ app/chapter0/funciones.py
@@ -0,0 +1,12 @@
+#!/bin/python
+# coding=utf-8
+
+def metodo():
+    pass
+
+def decir_hola():
+    print "Buongiorno, bambina!!"
+    
+def tam_cadena(cadena):
+    print "El tamaño de la cadena[",cadena,"] es: "
+    return len(cadena)
