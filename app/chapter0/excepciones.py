97ee161ca5d8ac3322ac24d9fcbed125125704c7
Fer_Carraro <carraro.fernando@gmail.com>
2019-08-25 18:32:50 -0500

    Se ha agregado excepciones.py

--- app/chapter0/excepciones.py
+++ app/chapter0/excepciones.py
@@ -2,8 +2,20 @@
 # coding=utf-8
 
 
+
+def excepcion_testB():
+	try:
+		import noExiste()
+	except Exception as e:
+		print "Ha ocurrido una excepcion: ",e
+	else:
+		print "Tal vez pase otra cosa"
+	finally:
+		print "Ha finalizado el bloque de ejecucion"
+
+
 def excepcion_testA():
-    try:
-       import noExiste()
+	try:
+		import noExiste()
     except e:
        print "Ha ocurrido una excepcion: ",e
