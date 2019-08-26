be0218dfa2a60476d0d639c4d0848e32f248d1dd
ARIEL <carraro.ariel@gmail.com>
2019-08-26 17:47:01 -0500

    Se edito bucles.py
    Se agregó método while

--- app/chapter0/bucles.py
+++ app/chapter0/bucles.py
@@ -3,9 +3,21 @@
 
 MAX = 100
 
+#ciclo for
 def ciclo_for():
     global MAX
     print "Ciclo for:"
     for i in range(1,MAX):
     	if i%3 == 0 and i%5 ==0:
     		print "No. ",i
+            
+#ciclo while return                   
+def ciclo_while_return():
+    global MAX
+    print "Ciclo while:"
+    contador, suma = 0,0
+    while contador < MAX:
+        suma += contador
+        contador += 3
+    return suma
+    
