29b539c9dad7ac48fdf21852b2edbb3cc5d05424
ARIEL <carraro.ariel@gmail.com>
2019-08-26 18:04:34 -0500

    Se editó entrada.py

--- app/chapter1/entrada.py
+++ app/chapter1/entrada.py
@@ -1,8 +1,16 @@
 #!/bin/python
 # coding=utf-8
 
-
-from PyZenity import InfoMessage
-from PyZenity import Question
 from PyZenity import ErrorMessage
 from PyZenity import GetText
+
+
+#GetText
+def introduce_dato():
+  numero=GetText(text='Introduce un número:', entry_text='', password=False)
+  try:
+    numero = int(numero)
+  except ValueError as e:
+    ErrorMessage('El formato no es el correcto: '+str(e))
+      
+   
