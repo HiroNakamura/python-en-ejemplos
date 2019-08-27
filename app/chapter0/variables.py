8b821b68f068fb073f28b006ccdde4cb2d4b3eb9
ARIEL <carraro.ariel@gmail.com>
2019-08-26 21:06:14 -0500

    Se edito variables.py

--- app/chapter0/variables.py
+++ app/chapter0/variables.py
@@ -20,3 +20,24 @@ def funcion_variables():
 	print "Constante global: ",TAM
 	print "Variable nula: ",nula
 	print "Variable if: ",variable
+	
+	
+def get_type():
+	global TAM
+	print "#Obteniedno tipos:"
+	entero = 33
+	character = 'A'
+	cadena = str("Python for Beginners")
+	booleana = 34 > 3 #True
+	booleana = not booleana #False
+	nula = None
+	variable = "Hola" if "A" in "AEIOU" else "Ciao"
+	print "Entero = ",type(entero)
+	print "Caracter = ",type(character)
+	print "Cadena = ",type(cadena)
+	print "Booleana = ",type(booleana) #False
+	print "Constante global: ",type(TAM)
+	print "Variable nula: ",type(nula)
+	print "Variable if: ",type(variable)
+	
+	
