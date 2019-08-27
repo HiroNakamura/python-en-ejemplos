4dcae8d4585903373cc3dfbcccfccc2ca7104a46
ARIEL <carraro.ariel@gmail.com>
2019-08-27 08:12:00 -0500

    Se edito archivos.py

--- app/chapter0/archivos.py
+++ app/chapter0/archivos.py
@@ -2,6 +2,14 @@
 # coding=utf-8
 
 
-ARCHIVO = ""
+ARCHIVO = "datos.csv"
+
+def leer_archivo():
+  global ARCHIVO
+  if open(ARCHIVO,"r"):
+    print "Parece que el archivo existe"
+  file = open(ARCHIVO,"r")
+  print type(file)
+  print "Contenido:",file.readlines()
 
 
