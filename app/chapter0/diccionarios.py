5323910beec8572b6a77b33a5f309362648970b1
ARIEL <carraro.ariel@gmail.com>
2019-08-27 11:13:15 -0500

    Se actualizó diccionarios.py

--- app/chapter0/diccionarios.py
+++ app/chapter0/diccionarios.py
@@ -11,4 +11,24 @@ def get_diccionario():
     diccionario["Tau"] = 400
     #Equivale a:
     #diccionario={"Alef":1,"Yod":10,"Tau":400}
-    return dicconario
+    return diccionario
+
+#Obtener llave-valor
+def get_dato_diccionario():
+    mi_diccionario = get_diccionario()
+    print "Diccionario: ",mi_diccionario,", tipo: ",type(mi_diccionario)
+    mi_diccionario["Beth"] = 2
+    mi_diccionario["Lamed"] = 30
+    for k in mi_diccionario:
+        print k," : ",mi_diccionario[k]
+    print "\n"
+    for key, value in d.iteritems():
+        print "Llave: ",key
+        print "Valor: ",value
+        
+        
+        
+        
+        
+        
+    
