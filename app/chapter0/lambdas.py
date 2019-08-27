c7213deaa39b923cba275433d143b24ef9e0531a
ARIEL <carraro.ariel@gmail.com>
2019-08-26 21:37:46 -0500

    Se editó lambdas.py

--- app/chapter0/lambdas.py
+++ app/chapter0/lambdas.py
@@ -3,4 +3,10 @@
 
 #Suma
 def get_lambdas():
-    pass
+    suma = lambda a,b : a+b
+    doble = lambda x : x*2
+    print "Suma(2,4) = ",suma(2,4)
+    print "Suma(-5,4) = ",suma(-5,4)
+    print "Doble de 2 es ",doble(2)
+    print "Doble de -45.8 es ",doble(-45.8)
+    
