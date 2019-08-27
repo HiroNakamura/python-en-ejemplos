5422fed9db8ed4f13421aa60ce75ee03808f93ac
ARIEL <carraro.ariel@gmail.com>
2019-08-26 22:45:49 -0500

    Se creó archivo selectivas.py

--- app/chapter0/selectivas.py
+++ app/chapter0/selectivas.py
@@ -3,4 +3,15 @@
 
 
 def menu():
-    pass
+    opcion = int(str('2'))
+    if opcion == 1:
+        print "Elegiste la opción 1"
+    elif opcion == 2:
+        print "Elegiste la opción 2"
+    else:
+        print "Tu opcion es: ",opcion
+        
+        
+def get_color_rojo(color):
+    color = "Rojo" if color == 'red' or color == 'Red' else "Tu color es "+color
+    return color
