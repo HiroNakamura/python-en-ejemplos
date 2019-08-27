42f392b81844227cd0b07612c44be09ccbae66c7
ARIEL <carraro.ariel@gmail.com>
2019-08-27 08:39:37 -0500

    Se creó diccionarios.py

---
+++ app/chapter0/diccionarios.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+#Diccionario
+def get_diccionario():
+    diccionario = {} # también diccionario = dict()
+    #agregar llave-valor
+    diccionario["Alef"] = 1
+    diccionario["Yod"] = 10
+    diccionario["Tau"] = 400
+    #Equivale a:
+    #diccionario={"Alef":1,"Yod":10,"Tau":400}
+    return dicconario
