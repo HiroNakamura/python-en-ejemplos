c857fc6a135d94b97abf0eec170c4fe44273ad1b
ARIEL <carraro.ariel@gmail.com>
2019-08-26 19:58:06 -0500

    Se edito directorio.py

--- app/chapter1/directorio.py
+++ app/chapter1/directorio.py
@@ -1,15 +1,28 @@
 #!/bin/python
 # coding=utf-8
 
-
 from PyZenity import ErrorMessage
 from PyZenity import GetDirectory
 from PyZenity import InfoMessage
-from PyZenity import Notification 
+from PyZenity import Notification
+from PyZenity import GetFilename
+
+
+#GetFilename
+def get_archivo():
+	print "#Get archivo:"
+	try:
+		archivo = GetFilename(multiple=True,sep='|')
+		InfoMessage('Archivo seleccionado:' +str(archivo),width=200,height=150,title='Archivo seleccionado')
+	except Exception as ex:
+		ErrorMessage('Ha ocurrido una excepcion: '+str(ex))
+	finally:
+		print "Ha finalizado el bloque"
 
 
 #GetDirectory
 def get_directorio():
+	print "#Get directorio:"
 	try:
 		directorio = GetDirectory(multiple=False,selected=None,sep=None)
 		InfoMessage('Tu selección es: '+str(directorio),width=200,height=150,title='Directorio seleccionado')
