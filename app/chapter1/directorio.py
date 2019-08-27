c74279d0ac22cb93856c283daaa03f5d8b00816e
ARIEL <carraro.ariel@gmail.com>
2019-08-26 20:57:49 -0500

    Se actualizó directorio.py

--- app/chapter1/directorio.py
+++ app/chapter1/directorio.py
@@ -14,6 +14,7 @@ def get_archivo():
 	try:
 		archivo = GetFilename(multiple=True,sep='|')
 		InfoMessage('Archivo seleccionado:' +str(archivo),width=200,height=150,title='Archivo seleccionado')
+		print "Archivo: ",str(archivo)
 	except Exception as ex:
 		ErrorMessage('Ha ocurrido una excepcion: '+str(ex))
 	finally:
@@ -27,6 +28,7 @@ def get_directorio():
 		directorio = GetDirectory(multiple=False,selected=None,sep=None)
 		InfoMessage('Tu selección es: '+str(directorio),width=200,height=150,title='Directorio seleccionado')
 		Notification(text='Tu selección es: '+str(directorio))
+		print "Directorio: ",str(directorio)
 	except Exception as ex:
 		ErrorMessage('Ha ocurrido una excepcion: '+str(e))
 	finally:
