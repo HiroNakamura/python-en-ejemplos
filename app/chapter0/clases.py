a338d8f43ce3d51ff133757ab7f2c0e88fdf75a4
ARIEL <carraro.ariel@gmail.com>
2019-08-27 11:20:17 -0500

    Actualizado clases.py

--- app/chapter0/clases.py
+++ app/chapter0/clases.py
@@ -4,4 +4,47 @@
 
 #Clase básica
 class Nodo(object):
-      pass
+      def __init__(self, valor,Nodo):
+            self.Nodo = Nodo
+            self.valor = valor
+            print "Se ha creado objeto Nodo"
+            
+      def __repr__(self):
+            return str(self.__dict__)
+
+#Clase base
+class Persona(object):
+    def __init__(self,nombre,apellidos,edad):
+        self.nombre = nombre 
+        self.apellidos = apellidos
+        self.edad = edad
+        print "Se creo e inicio objeto Persona"
+
+    def __repr__(self):
+        return str(self.__dict__)
+      
+      
+#Subclase de Persona     
+class Autor(Persona):
+    def __init__(self, nombre, apellidos,telefono):
+        Persona.__init__(self,nombre,apellidos)
+        self.telefono = telefono
+        print "Se creo e inicio objeto Autor"
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+
+class Libro(object):
+    def __init__(self, titulo, isbn,autor):
+        self.titulo = titulo
+        self.isbn = isbn
+        self.autor = autor 
+        print "Se ha creado e inicializado objeto Libro"
+
+    def __repr__(self):
+        return str(self.__dict__)
+      
+      
+ 
