2c7bb6c1a28174a2a12db187fda71a3ece6c3e0a
Fer_Carraro <carraro.fernando@gmail.com>
2019-08-26 19:52:32 -0500

    Se edito directorio.py

--- app/chapter1/directorio.py
+++ app/chapter1/directorio.py
@@ -5,13 +5,15 @@
 from PyZenity import ErrorMessage
 from PyZenity import GetDirectory
 from PyZenity import InfoMessage
+from PyZenity import Notification 
 
 
 #GetDirectory
 def get_directorio():
 	try:
 		directorio = GetDirectory(multiple=False,selected=None,sep=None)
-		InfoMessage('Tu selección es: '+str(dir),width=200,height=150,title='Directorio seleccionado')
+		InfoMessage('Tu selección es: '+str(directorio),width=200,height=150,title='Directorio seleccionado')
+		Notification(text='Tu selección es: '+str(directorio))
 	except Exception as ex:
 		ErrorMessage('Ha ocurrido una excepcion: '+str(e))
 	finally:
