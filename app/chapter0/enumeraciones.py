dd07c2ec966c7ae48ef5f2e35ad94da2301f4b3c
ARIEL <carraro.ariel@gmail.com>
2019-08-28 09:03:04 -0500

    Se ha creado enumeraciones.py

---
+++ app/chapter0/enumeraciones.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class PuntosCardinales:
+      NORTE = 0
+      SUR = 1
+      ESTE = 2
+      OESTE = 3
+     
+     
+class TAMANYO:
+      CHICO = 10.0
+      MEDIANO = 15.0
+      GRANDE = 22.0
+      
+     
