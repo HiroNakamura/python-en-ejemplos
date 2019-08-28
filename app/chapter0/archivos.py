5c873323c45a63e1bac86f63a9dc403ce35752e9
Fer_Carraro <carraro.fernando@gmail.com>
2019-08-28 17:26:51 -0500

    Se edito archivos.py

--- app/chapter0/archivos.py
+++ app/chapter0/archivos.py
@@ -2,14 +2,22 @@
 # coding=utf-8
 
 
-ARCHIVO = "datos.csv"
+import os
+
 
 def leer_archivo():
-  global ARCHIVO
-  if open(ARCHIVO,"r"):
-    print "Parece que el archivo existe"
-  file = open(ARCHIVO,"r")
-  print type(file)
-  print "Contenido:",file.readlines()
+	os.chdir(r'/home/fernando/Documentos/testRepositories/python-en-ejemplos/app/chapter0')
+	arch = open('datos.csv')
+	print "Ruta: ",open("datos.csv","r")
+	try:
+		if open("datos.csv","r"):
+			print "Parece que el archivo existe"
+	except IOError as e:
+		print "Error: ",e
 
-
+	try:
+		file = open("datos.csv","r")
+		print type(file)
+		print "Contenido:",file.readlines()
+	except IOError as e:
+		print "Error: ",e
