9250d1d972ee578b1155b82cd1655c2164cbd5d1
ARIEL <carraro.ariel@gmail.com>
2019-08-31 17:27:09 -0500

    Se actualizo botones.py
    Se ha añadido botones

--- app/chapter2/botones.py
+++ app/chapter2/botones.py
@@ -1,1 +1,19 @@
-#botones
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#import Tkinter
+from Tkinter import *
+import tkMessageBox
+
+
+def view_botones():
+  top = Tkinter.Tk()
+  btn = Tkinter.Button(top, text ="Python for Beginners", command = helloCallBack)
+  btn.pack()
+  top.mainloop()
+
+  
+
+def helloCallBack():
+   tkMessageBox.showinfo( "Python for Beginners", "Hola, mundo")
+
