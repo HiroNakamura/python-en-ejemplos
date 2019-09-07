11d944c065881ac9c1b1b91d98d5c76f3501dbda
ARIEL <carraro.ariel@gmail.com>
2019-09-07 18:57:49 -0500

    Se ha creado tuplas

---
+++ app/chapter0/tuplas.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def get_tupla():
+    tupla = () #tupla = tuple()
+    tupla = tupla + (1,)
+    tupla = tupla + (2,)
+    tupla = tupla + (3,4,5)
+    return tupla #(1,2,3,4)
