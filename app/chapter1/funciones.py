dd0d9f49bc9b01a04c89503cf5165c4b33c966cc
Fer_Carraro <carraro.fernando@gmail.com>
2019-09-08 20:19:20 -0500

    Se edito funciones.py

--- app/chapter1/funciones.py
+++ app/chapter1/funciones.py
@@ -9,4 +9,4 @@ def get_item_lista():
     item = lista()
     print "Elegiste: "
     print "Autor: ",item[0],", Lenguaje: ",item[1],", País:",item[2]
-    InfoMessage("Elegiste \nAutor: "+item[0]+"\nLenguaje: "+item[1]+"\nPaís:"+item[2],width=250,height=140,title="Python for Beginners")
+    InfoMessage("Elegiste: \nAutor: "+item[0]+"\nLenguaje: "+item[1]+"\nPaís:"+item[2],width=250,height=140,title="Python for Beginners")
