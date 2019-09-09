1596bfaebddcd69513a8600d2a5bad7d1a5441be
ARIEL <carraro.ariel@gmail.com>
2019-09-08 20:07:17 -0500

    Se ha editado funciones.py

--- app/chapter1/funciones.py
+++ app/chapter1/funciones.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from lista import *
+from PyZenity import InfoMessage
 
 
 def get_item_lista():
     item = lista()
     print "Elegiste: "
     print "Autor: ",item[0],", Lenguaje: ",item[1],", País:",item[2]
+    InfoMessage('Autor: '+item[0],', Lenguaje: '+item[1],', País:'+item[2],width=250,height=140,title="Python for Beginners")
