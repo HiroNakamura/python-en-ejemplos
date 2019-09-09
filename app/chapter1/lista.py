a8c94ef65d4beb66599e4c66a135dd72a8f9b24c
ARIEL <carraro.ariel@gmail.com>
2019-09-08 19:23:41 -0500

    Editado lista.py

--- app/chapter1/lista.py
+++ app/chapter1/lista.py
@@ -1,6 +1,10 @@
 #!/bin/python
 # coding=utf-8
 
+
+from PyZenity import List
+
+
 #List
 def lista():
-    return List(["Escoge","Autor","Lenguaje"], title="Selecciona un lenguaje", boolstyle="radiolist", editable=False, select_col="ALL", sep='|', data=[["","Guido","Python"],["","Gosling","Java"]])
+    return List(["Escoge","Autor","Lenguaje"], title="Selecciona un lenguaje", boolstyle="radiolist", editable=False, select_col="ALL", sep='|', data=[["","Guido van Rossum","Python"],["","James Gosling","Java"],["","Gavin King","Ceylon"]])
