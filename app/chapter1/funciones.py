202bcfaf51a795784f545e6937b439376431bdb6
Fer_Carraro <carraro.fernando@gmail.com>
2019-09-08 21:17:25 -0500

    Se ha editado funciones.py

--- app/chapter1/funciones.py
+++ app/chapter1/funciones.py
@@ -9,4 +9,17 @@ def get_item_lista():
     item = lista()
     print "Elegiste: "
     print "Autor: ",item[0],", Lenguaje: ",item[1],", País:",item[2]
-    InfoMessage("Elegiste: \nAutor: "+item[0]+"\nLenguaje: "+item[1]+"\nPaís:"+item[2],width=250,height=140,title="Python for Beginners")
+    autor = item[0]
+
+    if autor == 'Larry Wall':
+    	autor = autor +", \n'Los verdaderos programadores pueden escribir código ensamblador en cualquier lenguaje.'"
+    elif autor == 'James Gosling':
+    	autor = autor +" \n'La imitación es la forma más sincera de adulación, muchas gracias.'"
+    else: 
+    	if autor == "Guido van Rossum":
+    		autor = autor +" \n'Python es mejor que cualquier otro lenguaje.'"
+    	else:
+    		autor = autor +" \n'Hola.'"
+
+
+    InfoMessage("Elegiste: \nAutor: "+autor+"\nLenguaje: "+item[1]+"\nPaís:"+item[2],width=250,height=140,title="Python for Beginners")
