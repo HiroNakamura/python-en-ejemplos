f2e8bcc89f5c48610d7040da5be922aa1d395776
ARIEL <carraro.ariel@gmail.com>
2019-09-08 19:32:52 -0500

    Se actualizó lista.py

--- app/chapter1/lista.py
+++ app/chapter1/lista.py
@@ -7,4 +7,10 @@ from PyZenity import List
 
 #List
 def lista():
-    return List(["Escoge","Autor","Lenguaje"], title="Selecciona un lenguaje", boolstyle="radiolist", editable=False, select_col="ALL", sep='|', data=[["","Guido van Rossum","Python"],["","James Gosling","Java"],["","Gavin King","Ceylon"]])
+    return List(["Escoge","Autor","Lenguaje","País"], 
+                title="Selecciona un lenguaje", boolstyle="radiolist", 
+                editable=False, select_col="ALL", sep='|', data=[["","Guido van Rossum","Python","Países Bajos"]
+                                                                 ,["","Anders Hejlsberg","C#","Dinamarca"]
+                                                                 ,["","James Gosling","Java","Canadá"]
+                                                                 ,["","Larry Wall","Perl","USA"]
+                                                                 ,["","Gavin King","Ceylon","Unknow"]])
