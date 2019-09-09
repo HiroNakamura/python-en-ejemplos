ef9b4333601535505eeaaba322d25e2cc9bad77c
ARIEL <carraro.ariel@gmail.com>
2019-09-08 20:08:56 -0500

    funciones.py editado

--- app/chapter1/funciones.py
+++ app/chapter1/funciones.py
@@ -9,4 +9,4 @@ def get_item_lista():
     item = lista()
     print "Elegiste: "
     print "Autor: ",item[0],", Lenguaje: ",item[1],", País:",item[2]
-    InfoMessage('Autor: '+item[0],', Lenguaje: '+item[1],', País:'+item[2],width=250,height=140,title="Python for Beginners")
+    InfoMessage("Autor: "+item[0]+", Lenguaje: "+item[1]+", País:"+item[2],width=250,height=140,title="Python for Beginners")
