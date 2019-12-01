205fdf5a2e9b96eb2fa7b54f58c3f2b49ae1a0f6
Fer_Carraro <carraro.fernando@gmail.com>
2019-11-30 20:22:12 -0600

    Se ha modificado el archivo file1.py

--- app/chapter3/file1.py
+++ app/chapter3/file1.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-file = open("datos.txt","r")
-for line in file:
-  print line
+'''
+Fer Carraro
+@2019
+'''
+
+
+def main():
+	msg = "Existe el archivo"
+	print "Manejo de archivos:"
+	try:
+		file = open("datos.txt","r")
+		existe = True if file != None else False
+	except IOError as error:
+		print error
+		existe = False
+		msg = "No existe el archivo"
+	finally:
+		print msg
+		if existe:
+			print "Contenido:"
+			for line in file:
+				print line
+
+
+if __name__ == '__main__':
+	main()
