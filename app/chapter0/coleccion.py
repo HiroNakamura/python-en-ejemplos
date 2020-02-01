99017bd10432f2e096d657d6cf5acb5ec7b24bb0
ARIEL <carraro.ariel@gmail.com>
2020-02-01 08:27:20 -0600

    Se creó archivo coleccion.py
    Un ejemplo de Set, cuya característica es que no se permiten elementos repetidos

---
+++ app/chapter0/coleccion.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def coleccion():
+    set_1 = set()
+    print set_1
+    set_2 = set([1,2,3,4,5])
+    print set_2
+    set_3 = {1,2,3,4,}
+    print set_3
+    
+    
