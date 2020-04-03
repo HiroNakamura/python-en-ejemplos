3a1844104c9c2a85532a503c42aa7b8f66fc0fdc
ARIEL <carraro.ariel@gmail.com>
2020-04-02 19:42:55 -0600

    coleccion.py

--- app/chapter0/coleccion.py
+++ app/chapter0/coleccion.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#Uso de Set
 def coleccion():
     set_1 = set()
-    print set_1
+    print(set_1)
     set_2 = set([1,2,3,4,5])
-    print set_2
+    print(set_2)
     set_3 = {1,2,3,4,}
-    print set_3
+    print(set_3)
     
     
