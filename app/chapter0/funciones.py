1e94f48c490bcb0c9323228522669e51cc24497e
ARIEL <carraro.ariel@gmail.com>
2020-04-20 22:00:11 -0500

    funciones.py
    Se ha editado funciones.py

--- app/chapter0/funciones.py
+++ app/chapter0/funciones.py
@@ -9,6 +9,12 @@ from listas import *
 from diccionarios import *
 
 
+def suma(x, y):
+    return x + y
+
+def entrada():
+    print("Suma: ",suma(int(input("x: ")),int(input("y: ")))
+
 
 def metodo():
     pass
@@ -21,22 +27,21 @@ def tam_cadena(cadena):
     return len(cadena)
 
 
-
 def mockTestB():
-	print("Tupla:\n\t",get_tupla())
-	get_other_tupla()
-	funciones_lista()
-	funciones_tupla()
-	funciones_diccionario()
-	lista = [32,65,87,201,302,604,703,832,991]
-	print("Lista: ",lista)
-	print("Pares: ",get_pares(lista))
-	print("Impares: ",get_impares(lista))
-	print("Ficheros .py:")
-	listar_ficheros()
-	nombres = ['ana','fernando','miguel','laura','juana','hortencia',' ']
-	print(nombres)
-	print(listar_capitalize(nombres))
+    print("Tupla:\n\t",get_tupla())
+    get_other_tupla()
+    funciones_lista()
+    funciones_tupla()
+    funciones_diccionario()
+    lista = [32,65,87,201,302,604,703,832,991]
+    print("Lista: ",lista)
+    print("Pares: ",get_pares(lista))
+    print("Impares: ",get_impares(lista))
+    print("Ficheros .py:")
+    listar_ficheros()
+    nombres = ['ana','fernando','miguel','laura','juana','hortencia',' ']
+    print(nombres)
+    print(listar_capitalize(nombres))
 
 
 def mockTestA():
