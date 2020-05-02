33ba4290d72fcb6698bd0a98e1efae83f2f2e1c9
ARIEL <carraro.ariel@gmail.com>
2020-05-02 16:34:02 -0500

    tortuga.py
    Archiv tortuga.py creado

---
+++ temario/chapter0/tortuga.py
@@ -0,0 +1,20 @@
+#!/bin/python
+# coding=utf-8
+
+from turtle import *
+#import turtle
+
+def testA():
+  wn = turtle.Screen()
+  wn.bgcolor("light green")
+  wn.title("Turtle")
+  skk = turtle.Turtle()
+  skk.forward(100)
+  turtle.done()
+
+
+def main():
+	testA()
+
+if __name__ == '__main__':
+    main()
