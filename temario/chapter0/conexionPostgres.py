c77773f603108dab2e70062e22b80520e96bf9a4
Fer_Carraro <carraro.fernando@gmail.com>
2020-05-11 20:18:55 -0500

    Se ha creado archivo database.ini y editado conexionPostgres.py

--- temario/chapter0/conexionPostgres.py
+++ temario/chapter0/conexionPostgres.py
@@ -7,12 +7,8 @@ import psycopg2
 
 
 def config(filename='database.ini', section='postgresql'):
-    # create a parser
     parser = ConfigParser()
-    # read config file
     parser.read(filename)
-
-    # get section, default to postgresql
     db = {}
     if parser.has_section(section):
         params = parser.items(section)
@@ -20,27 +16,26 @@ def config(filename='database.ini', section='postgresql'):
             db[param[0]] = param[1]
     else:
         raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
     return db
     
 
 
 def connect():
-    """ Connect to the PostgreSQL database server """
+    """ Conexion a la base de datos PostgreSQL """
     conn = None
     try:
         # read connection parameters
         params = config()
 
         # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
+        print('Conectando a la base de datos PostgreSQL...')
         conn = psycopg2.connect(**params)
 		
         # create a cursor
         cur = conn.cursor()
         
 	# execute a statement
-        print('PostgreSQL database version:')
+        print('Version de la base de datos PostgreSQL:')
         cur.execute('SELECT version()')
 
         # display the PostgreSQL database server version
@@ -54,7 +49,7 @@ def connect():
     finally:
         if conn is not None:
             conn.close()
-            print('Database connection closed.')
+            print('La conexion a la base de datos PostgreSQL.')
 
 
 
