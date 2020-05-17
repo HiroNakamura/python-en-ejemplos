fb2e9b59f8cc77ec5201ac66a80e5468bec1a427
Fer_Carraro <carraro.fernando@gmail.com>
2020-05-17 17:06:37 -0500

    Se ha editado mensajes-py

--- temario/chapter6/app/mensajes.py
+++ temario/chapter6/app/mensajes.py
@@ -13,18 +13,49 @@ class Mensajes(Document):
     mensaje = StringField(required = True)
 
 
-def main():
+
+def get_mensajes():
+    print("Mensajes:")
+    mensajes = Mensajes.objects()
+    if mensajes != None:
+        for mensaje in mensajes:
+            print("Id: ",mensaje.id)
+            print("Idioma: ",mensaje.idioma)
+            print("Mensaje: ",mensaje.mensaje)
+    else:
+        print("No hay mensajes")
+        
+
+
+def crear(idioma, mensaje):
     hecho = 'Hecho. Documento guardado.'
     try:
         mensajes = Mensajes(
             id = ObjectId(),
-            idioma = 'es_ES',
-            mensaje = 'Morituri te salutan').save()
+            idioma = idioma,
+            mensaje = mensaje).save()
         print(mensajes)
     except NameError as error:
         hecho = 'Ha ocurrido un error: '+str(error)
     finally:
         print(hecho)
+
+
+def get_objeto(myobj):
+    return "Mensaje{\n\tid:"+str(myobj.id)+",\n\tidioma:" +myobj.idioma + ",\n\tmensaje: "+myobj.mensaje+"\n}\n"
+
+
+def main():
+    crear('de_DE','Guten Tag!')
+    get_mensajes()
+    italiano = None
+    try:
+        italiano = Mensajes.objects(idioma='it_IT').get()
+    except TypeError as error:
+        italiano = "Error ocurrido: "+str(error)
+    finally:
+        print("Lo que se ha obtenido es:")
+        print(get_objeto(italiano))
 
 
 if __name__ == '__main__':
