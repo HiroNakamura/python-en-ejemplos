fd4b242c6be12bf2fc4c61272d2af491da34c6d3
HIRONAKAMURA <carraro.ariel@gmail.com>
2022-04-24 16:11:45 -0500

    Se sube nuevo archivo: curso.py

---
+++ temario/chapter0/curso.py
@@ -0,0 +1,26 @@
+#!/bin/python
+# coding=utf-8
+'''
+Curso: Python TOTAL -Programador Avanzado en 16 días
+'''
+
+
+
+
+
+
+# 2. Uso de eval()
+x,y = 3,5
+suma = eval("x+y")
+print(f"{x} + {y} = {suma}")
+division = eval("x/y")
+print(f"{x} / {y} = {division}")
+
+# 1. Uso de print
+'''
+print("Hola, mundo")
+numero=33
+cadena="11 veces 11"
+print(f"El numero {numero} x 3 da {numero*3}")
+print(f"{cadena}")
+'''
